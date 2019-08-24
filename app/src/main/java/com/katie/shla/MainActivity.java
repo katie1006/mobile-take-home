b044c66eb877d073615a04443aad9769eeda7ccc
Katie Liu <katie.yiliu@gmail.com>
2019-08-24 16:20:35 -0400

    Android project created.

---
+++ app/src/main/java/com/katie/shla/MainActivity.java
@@ -0,0 +1,16 @@
+package com.katie.shla;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
