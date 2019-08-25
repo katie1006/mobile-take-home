9edaf1505806f6a90d122d1fac382f1d819a7b3e
Katie Liu <katie.yiliu@gmail.com>
2019-08-25 19:20:13 -0400

    Updated episode list UI.

--- app/src/main/java/com/katie/shla/episode/EpisodeViewHolder.java
+++ app/src/main/java/com/katie/shla/episode/EpisodeViewHolder.java
@@ -12,14 +12,23 @@ import com.katie.shla.utils.list.ListViewHolder;
 public class EpisodeViewHolder extends ListViewHolder<Episode> {
 
     private final TextView name;
+    private final TextView episode;
+    private final TextView airDate;
+    private final TextView numCharacters;
 
     public EpisodeViewHolder(@NonNull View root) {
         super(root);
         name = root.findViewById(R.id.name);
+        episode = root.findViewById(R.id.episode);
+        airDate = root.findViewById(R.id.airDate);
+        numCharacters = root.findViewById(R.id.numCharacters);
     }
 
     @Override
     public void bind(Episode data) {
         name.setText(data.name);
+        episode.setText(data.episodeCode);
+        airDate.setText(data.airDate);
+        numCharacters.setText(numCharacters.getResources().getString(R.string.num_character, data.charUrls.length));
     }
 }
